8e144f7dacc3d72a341a49dfb0e6255d7d313518
federicodallari27 <52299834+federicodallari27@users.noreply.github.com>
2020-12-04 10:26:58 +0100

    Update gender-percentages.sql
    no nested queries

--- Chapter4/gender-percentages.sql
+++ Chapter4/gender-percentages.sql
@@ -1,3 +1,4 @@
-SELECT gender, (COUNT(gender) * 100.0 / (SELECT COUNT(*) FROM <AWS_GLUE_TABLE_NAME>)) AS percent
-FROM <AWS_GLUE_TABLE_NAME>
-GROUP BY gender;
+SELECT gender, (COUNT(*) * 100.0 / sum(COUNT(*)) OVER ()) AS percent
+FROM   <AWS_GLUE_TABLE_NAME>
+GROUP  BY gender
+ORDER  BY percent;                                        
